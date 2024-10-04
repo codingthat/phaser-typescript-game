d9b72f739c7f6e93bc4fadffe0700c7417983dfa
Kevin Bloch <kev@codingthat.com>
2024-10-04 14:42:27 +0200

    Add FPS counter

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,9 +1,13 @@
 import Phaser from 'phaser';
 import Player from '../objects/Player';
 
+type Text = Phaser.GameObjects.Text;
+
+
 export default class GameScene extends Phaser.Scene {
     private player: Player;
     private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    private fpsText: Text;
 
     constructor() {
         super({ key: 'GameScene' });
@@ -11,10 +15,16 @@ export default class GameScene extends Phaser.Scene {
 
     create(): void {
         this.player = new Player(this, 0, 0);
+
+        this.fpsText = this.add.text(10, 10, '', {
+            color: '#00ff00'
+        });
+
         this.cursors = this.input.keyboard!.createCursorKeys();
     }
 
     update(): void {
+        this.fpsText.setText('FPS: ' + Math.floor(this.sys.game.loop.actualFps));
         this.player.handleMovement(this.cursors.left.isDown, this.cursors.right.isDown, this.cursors.up.isDown);
     }
 }
